6720cb046cc9e16e73dba01c49b635f907180e18
Naveen Dwivedi <naveen007.1994@gmail.com>
2021-05-24 18:08:11 +0530

    master

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { api_key } from '../apikey'
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  api_key= '449954af0df34f1a8a241e78ea2acf9f'
+  api_key= api_key
   constructor( private http:HttpClient ) {}
  getCountryheadlines(country:string){
    return this.http.get("https://newsapi.org/v2/top-headlines?country="+ country+"&apiKey="+this.api_key)
